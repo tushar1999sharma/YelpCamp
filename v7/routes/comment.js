bd9a8d13199008c5335b1596faee0c92ef63c3ba
tushar1999sharma <59363044+tushar1999sharma@users.noreply.github.com>
2020-02-28 20:09:14 +0530

    Add files via upload

---
+++ v7/routes/comment.js
@@ -0,0 +1,47 @@
+var express         = require("express"),
+    router          = express.Router(),
+    campGround      = require("./models/campground"), //camGround schema
+    Comment         = require("./models/comment"), //comment schema
+    User            = require("../models/user"); //user Schema
+
+router.get("/new", isLoggedIn, function(req,res){ //middleware will check if user logged in or not
+    campGround.findById(req.params.id,function(err,campground){
+        if(err){
+            console.log(err);
+        }
+        else{
+            //console.log(campground);
+            res.render("../views/comments/new.ejs",{campground: campground});
+        }
+    })
+})
+
+router.post("", isLoggedIn, function(req, res){ //middleware will check if user logged in or not
+    campGround.findById(req.params.id,function(err,campground){
+        if(err){
+            console.log(err);
+        }
+        else{
+            Comment.create(req.body.comment,function(err,comment){
+                if(err) {
+                    console.log(err);
+                }
+                else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    console.log(campground);
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
+        }
+    })
+})
+
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports = router;
